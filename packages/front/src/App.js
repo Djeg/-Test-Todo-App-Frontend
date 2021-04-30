a6954b36d17ac9606f7d803108d0b50b0ac7fd17
David Jegat <david.jegat@knplabs.com>
2021-04-30 15:44:04 +0200

    Finish todo app

--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from './App.module.css';
 
 const App = () => {
   const [todoOpen, setTodoOpen] = useState(false);
   const [todos, setTodos] = useState([]);
 
+  useEffect(async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/todos`);
+    const todos = await response.json();
+
+    setTodos(todos);
+  }, [])
+
   const whenTitleEnd = (ev) => setTodoOpen(true);
-  const addTodo = (e) => {
+  const addTodo = async (e) => {
     e.preventDefault();
 
-    setTodos([
-      ...todos,
-      {
-        text: e.target.text.value,
-        complete: false,
-      }
-    ]);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/todos`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify([
+        { text: e.target.text.value, complete: false }
+      ]),
+    })
+    const todos = await response.json();
+
+    setTodos(todos);
+
   }
-  const completeTodo = (i) => () => setTodos(
-    todos.map((t, i2) =>
-      i2 === i
-      ? { ...t, complete: !t.complete }
-      : t
-    )
-  )
-  const removeTodo = (i) => (e) => {
+  const completeTodo = (todo) => async () => {
+    todo.complete = !todo.complete;
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/todos/${todo._id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(todo),
+    });
+    const todos = await response.json();
+
+    setTodos(todos);
+  }
+  const removeTodo = (todo) => async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    setTodos(
-      todos.filter((t, i2) => i2 !== i)
-    )
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/todos/${todo._id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const todos = await response.json();
+
+    setTodos(todos);
   }
-  const completeAll = () =>
-    setTodos(todos.map(t => ({ ...t, complete: !t.complete })))
-  const removeAllTodo = () => setTodos([])
 
   return (
     <div className={classNames.app}>
@@ -45,17 +62,13 @@ const App = () => {
             <input type="text" placeholder="Votre tâche" name="text" />
             <button type="submit">Ajouter une tache</button>
           </form>
-          <div className={classNames.btnGroup}>
-            <button onClick={completeAll}>Tout cocher</button>
-            <button onClick={removeAllTodo}>Tout Supprimer</button>
-          </div>
           <div className={classNames.todoCard}>
             {todos.length === 0 && <p>Vous n'avez plus aucune taches :-)</p>}
             {todos.length > 0 && <p>Vous avez {todos.length} tâches</p>}
             {(todos.map((todo, i) => 
-              <div className={`${classNames.todoEntry} ${todo.complete ? classNames.complete : ''}`} onClick={completeTodo(i)} key={`todo-${i}`}>
+              <div className={`${classNames.todoEntry} ${todo.complete ? classNames.complete : ''}`} onClick={completeTodo(todo)} key={`todo-${i}`}>
                 <p>{todo.text}</p>
-                <button onClick={removeTodo(i)}>Supprimer</button>
+                <button onClick={removeTodo(todo)}>Supprimer</button>
               </div>
             ))}
           </div>
